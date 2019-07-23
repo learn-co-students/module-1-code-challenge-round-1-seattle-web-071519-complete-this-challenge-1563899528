b0e926c578c1dded21bb30058a9bf23bf38d15cd
Ellyn <eka249@nyu.edu>
2019-07-23 10:40:02 -0700

    Mod 1 Code Challenge try #1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,64 @@
 class Customer
   attr_reader :first_name, :last_name
+  @@all=[]
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  #revise to print out content, not id
+  #edit broke this method
+  def add_review(restaurant, content, rating)
+    Review.new(review, self, rating)
+  end
+
+  #done
+  def num_reviews
+    Review.all.map do |reviews|
+      reviews.customer == self
+    end
+  end
+
+  #NameError: uninitialized constant Customer::Reviews
+  # def allreviews
+  #   Reviews.all.collect do |review|
+  #     review.customer==self
+  #   end
+  # end
+
+  def reviews
+    allreviews.select do |reviews|
+      reviews.length == self
+    end
+  end
+
+
+
+  def self.find_by_name(name)
+    @@all.find do |cust|
+      cust.first_name == name
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    @@all.select do |cust|
+      cust.first_name== name
+    end
+  end
+
+  def self.all_names
+    @@all.first_name
+  end
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,30 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
+
+puts pizzahut= Restaurant.new("Pizza Hut")
+tacoshop= Restaurant.new("Taco Shop")
+sonic= Restaurant.new("Sonic")
+dunkin= Restaurant.new("Dunkin Donuts")
+
+
+puts leo=Customer.new("Leonardo", "Dicaprio")
+mark= Customer.new("Mark", "Hamill")
+obama= Customer.new("Barak", "Obama")
+
+#puts review1= Review.new("testing", leo, 4)
+
+puts Customer.find_all_by_first_name("Mark")
+
+
+#puts leo.num_reviews
+
+#puts addreview1= leo.add_review(pizzahut, "testing2", 1)
+
+
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
 binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+#leave this here to ensure binding.pry isn't the last line
